bf9f5b7e2f14fcd7b12efbe1277e1c1976eda559
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-05-02 18:41:33 +0700

    image

--- src/components/containers/DefaultLayout/Avatar.tsx
+++ src/components/containers/DefaultLayout/Avatar.tsx
@@ -6,7 +6,7 @@ class Avatar extends Component<any, any> {
     const {id, name} = this.props;
     return (
       <Container className="text-align-center mar-top-15">
-        <img src="../../assets/img/avatars/3.jpg" className="img-avatar mar-bot-10" style={{width:60}} alt="avatar"/>
+        {/* <img src="../../assets/img/avatars/3.jpg" className="img-avatar mar-bot-10" style={{width:60}} alt="avatar"/> */}
         <h6>{name?name: 'MEMBER NAME' }</h6>
         <p>{id? id: 'MEMBER ID'}</p>
       </Container>
